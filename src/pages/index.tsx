2456703f74f7a6c62401750ee60bdc7ef005d3f9
whil <miltonivan117@gmail.com>
2022-02-01 22:27:00 -0600

    landing refacor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,29 @@
-import {
-  LadingPageWrapper,
-  LandingPageButton,
-  LandingPageContent,
-  LandingPageImg,
-} from '@Styles/pages'
+import * as S from '@Styles/pages'
 import Svg from '@Whil/components/Svg'
 import { NextPage } from 'next'
-
 import { signIn } from 'next-auth/react'
 
-type Props = {}
-
-const LandingPage: NextPage<Props> = () => {
-  const handleLogin = () => {
-    signIn('spotify', { callbackUrl: 'http://localhost:3000/home' })
-  }
-  return (
-    <LadingPageWrapper>
-      <Svg
-        src="/image/landing/spotify"
-        style={{ p0sition: 'fixed', margin: '-100px -40px' }}
-      />
-      <LandingPageContent>
-        <h1>Welcome back!</h1>
-        <h3>Sign in with spotify</h3>
-        <LandingPageButton onClick={handleLogin}>Sign In</LandingPageButton>
-      </LandingPageContent>
-      <footer>
-        <LandingPageImg src="/image/landing/landingbg.png" alt="landingbg" />
-      </footer>
-    </LadingPageWrapper>
-  )
-}
+const LandingPage: NextPage = () => (
+  <S.LadingPageWrapper>
+    <Svg
+      src="/image/landing/spotify"
+      style={{ p0sition: 'fixed', margin: '-100px -40px' }}
+    />
+    <S.LandingPageContent>
+      <h1>Welcome back!</h1>
+      <h3>Sign in with spotify</h3>
+      <S.LandingPageButton
+        onClick={() => {
+          signIn('spotify', { callbackUrl: 'http://localhost:3000/home' })
+        }}
+      >
+        Sign In
+      </S.LandingPageButton>
+    </S.LandingPageContent>
+    <footer>
+      <S.LandingPageImg src="/image/landing/landingbg.png" alt="landingbg" />
+    </footer>
+  </S.LadingPageWrapper>
+)
 
 export default LandingPage
