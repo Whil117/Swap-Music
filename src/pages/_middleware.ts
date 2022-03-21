e3e8b540da0d0e61f44aad10d91c1972bba78e91
Ivan García <67771688+Whil117@users.noreply.github.com>
2022-03-20 20:28:21 -0600

    middleware

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -5,15 +5,15 @@ export default async function middleware(req: any) {
     req,
     secret: process.env.NEXTAUTH_SECRET as string,
   })
-  // const { pathname } = req.nextUrl
+  const { pathname } = req.nextUrl
 
-  // if (pathname === '/' && token) {
-  //   return NextResponse.redirect('/swap')
-  // }
-  // if (pathname.includes('/api/auth') || token) return NextResponse.next()
-  // const url = req.nextUrl.clone()
-  // url.pathname = '/'
-  // if (pathname.includes('/swap') && !token) {
-  //   return NextResponse.redirect(url.toString())
-  // }
+  if (pathname === '/' && token) {
+    return NextResponse.redirect('/swap')
+  }
+  if (pathname.includes('/api/auth') || token) return NextResponse.next()
+  const url = req.nextUrl.clone()
+  url.pathname = '/'
+  if (pathname.includes('/swap') && !token) {
+    return NextResponse.redirect(url.toString())
+  }
 }
