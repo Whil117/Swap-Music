0e3d6200a54b18a5be3f6d620b0fbd660605001d
Ivan García <67771688+Whil117@users.noreply.github.com>
2022-06-20 22:42:00 -0500

    atomseopage update component

--- src/lib/AtomSeo/index.tsx
+++ src/lib/AtomSeo/index.tsx
@@ -1,106 +1,91 @@
 import Head from 'next/head'
 import { FC } from 'react'
 
-export interface SeoProps {
-  page?: string
+export interface SeoPageProps {
   title?: string
+  page?: string
+  website?: string
+  description?: string
+  keywords?: string[]
   icon?: string
   image?: string
-  description?: string
-  website?: string
-  sameAs?: string[]
-  name?: string
-  alternateName?: string
-  keywords?: string[]
+  locale?: string
+  json?: string
+  jsonType?: string
+  children?: React.ReactNode
 }
 
-const AtomSeoLayout: FC<SeoProps> = ({
-  children,
-  title,
-  icon,
-  website,
-  image,
-  keywords,
-  description,
-  name,
-  alternateName,
-  sameAs,
-}) => {
+const AtomSeoLayout: FC<SeoPageProps> = (props) => {
   return (
     <>
       <Head>
-        <title>Swap | {title}</title>
-        <link rel="icon" type="image/png" href={icon || `/favicon.png`} />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
+        <title>
+          {props.title} | {props.page}
+        </title>
+        <link rel="icon" type="image/png" href={props.icon ?? `/favicon.png`} />
         <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=7" />
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
-        <meta name="canonical" content={website} />
+        <meta name="canonical" content={props.website} />
         <meta
           name="keywords"
-          content={keywords ? keywords.join(`, `) : `nextjs, typescript`}
+          content={
+            props.keywords ? props.keywords.join(`, `) : `nextjs, typescript`
+          }
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#f80000" />
-        <link rel="apple-touch-icon" href={image} />
-        <meta name="apple-mobile-web-app-status-bar" content="#90cdf4" />
-        <meta name="description" content={description} />
-        <meta name="author" content="Stackly Code" />
-        <meta name="copyright" content="Stackly Code" />
-        <meta name="title" content={title} />
-        <meta name="description" content={description} />
+        <meta name="description" content={props.description} />
+        <meta name="author" content="Ixulabs" />
+        <meta name="copyright" content="Ixulabs" />
+        <meta name="title" content={props.title} />
+        <meta name="description" content={props.description} />
         <meta name="googlebot" content="index,follow" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content={website} />
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} />
-        <meta property="og:image" content={image} />
+
+        <meta property="og:locale" content={props.locale ?? 'es_ES'} />
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content={props.title} />
+        <meta property="og:description" content={props.description} />
+        <meta property="og:url" content={props.website} />
+        <meta
+          property="og:site_name"
+          content={`${props.title} | ${props.page}`}
+        />
+        <meta property="og:image" content={props.image || `/preview.png`} />
+
+        <meta
+          property="og:image:secure_url"
+          content={props.image || `/preview.png`}
+        />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:image:alt" content={props.title} />
+
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:url" content={website} />
-        <meta name="twitter:title" content={title} />
-        <meta name="twitter:description" content={description} />
-        <meta name="twitter:image" content={image} />
-        <link rel="manifest" href="manifest.webmanifest" />
+        <meta name="twitter:description" content={props.description} />
+        <meta name="twitter:title" content={props.title} />
+        <meta name="twitter:image" content={props.image || `/preview.png`} />
+
+        <meta
+          property="og:image"
+          itemProp="image"
+          content={props.image || `/preview.png`}
+        />
+        <meta
+          property="og:image:secure_url"
+          content={props.image || `/preview.png`}
+        />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="300" />
+        <meta property="og:image:height" content="300" />
         <script
-          type="application/ld+json"
+          type={props.jsonType ?? 'application/ld+json'}
           dangerouslySetInnerHTML={{
-            __html: JSON.stringify({
-              '@context': `https://schema.org`,
-              '@type': `Organization`,
-              name,
-              alternateName,
-              url: website || image,
-              logo: `${website}`,
-              contactPoint: {
-                '@type': `ContactPoint`,
-                telephone: ``,
-                contactType: `customer service`,
-                areaServed: [`AR`, `CO`, `MX`, `UY`, `SV`, `DO`],
-                availableLanguage: [`en`, `es`],
-              },
-              sameAs,
-            }),
+            __html: props?.json ?? '',
           }}
         />
       </Head>
-      {children}
+      {props?.children}
     </>
   )
 }
