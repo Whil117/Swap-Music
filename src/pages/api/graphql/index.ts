b493367c42292a82a2a364ec09e86bcc845025d8
Ivan García <67771688+Whil117@users.noreply.github.com>
2022-06-25 17:12:06 -0500

    config

--- src/pages/api/graphql/index.ts
+++ src/pages/api/graphql/index.ts
@@ -1,15 +1,28 @@
 import { ApolloServer, makeExecutableSchema } from 'apollo-server-micro'
+import { NextApiRequest, NextApiResponse } from 'next'
 import connectMongoDB from '../../../db/index'
 import resolvers from '../../../db/resolvers'
 import typeDefs from '../../../db/types'
 
 connectMongoDB()
+
 const apolloServer = new ApolloServer({
   schema: makeExecutableSchema({
     typeDefs,
     resolvers,
   }),
 })
+const startServer = apolloServer.start()
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  await startServer
+  await apolloServer.createHandler({
+    path: '/api/graphql',
+  })(req, res)
+}
 
 export const config = {
   api: {
@@ -17,4 +30,4 @@ export const config = {
   },
 }
 
-export default apolloServer.createHandler({ path: '/api/graphql' })
+// export default apolloServer.createHandler({ path: '/api/graphql' })
