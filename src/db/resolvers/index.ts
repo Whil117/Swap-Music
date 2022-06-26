a6305c632f3418db333acd1835c98233caf2309b
Ivan García <67771688+Whil117@users.noreply.github.com>
2022-06-26 00:25:09 -0500

    type

--- src/db/resolvers/index.ts
+++ src/db/resolvers/index.ts
@@ -4,14 +4,14 @@ const Artist = require('../models/artist')
 const resolvers = {
   Query: {
     // products
-    listArtist: async (_, { filter }) => {
+    listArtist: async (_: any, { filter }: any) => {
       const artists = await Artist.find({ ...filter })
       return artists
     },
   },
 
   Mutation: {
-    createArtist: async (_, { input }) => {
+    createArtist: async (_: any, { input }: any) => {
       const artist = new Artist(input)
       const result = await artist.save()
       return result
